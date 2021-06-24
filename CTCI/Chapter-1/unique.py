1aab2bf8ac6144f000501b03bbe5e495ead58955
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-06-23 21:42:26 -0400

    isUnique.py
    Chapter 1 - 1.1

---
+++ CTCI/Chapter-1/unique.py
@@ -0,0 +1,32 @@
+## Check if all elements of the string are unique
+
+# Using multiple DS
+def isUnique(string):
+  ele = []
+  for i in string:
+    ele.append(i)
+  if len(ele) == len(set(ele)):
+    return True
+  return False
+
+# Using one DS - Hash Map
+def isUnique(string):
+    ele = {}
+    for i in string:
+        if i in ele.keys():
+            ele[i] += 1
+        else:
+            ele[i] = 1
+    if ele[i] > 1:
+        return False
+    return True
+
+# Using one DS - Array
+def unique(string):
+    ele = []
+    for i in string:
+        if i in ele:
+            return False
+        ele.append(i)
+    return True
+
