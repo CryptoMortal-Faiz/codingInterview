a48de93e0c8cccecc5b34a93f279b42d06e2fd71
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-07-05 12:47:54 -0500

    Create palindrome.py

---
+++ CTCI/Chapter-1/palindrome.py
@@ -0,0 +1,8 @@
+# Check if given string is a palindrome
+
+def palindrome(str):
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
+print(palindrome("malam"))
