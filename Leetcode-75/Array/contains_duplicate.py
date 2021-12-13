5f775c9e26302e3144b2f75a0f9258aac835cb02
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-12-13 08:36:33 -0600

    Update contains_duplicate.py

--- Leetcode-75/Array/contains_duplicate.py
+++ Leetcode-75/Array/contains_duplicate.py
@@ -1,5 +1,6 @@
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+# Hashmap
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         occurence = dict()
@@ -11,3 +12,11 @@ class Solution:
                 return True
             else:
                 occurence[i] = initial
+
+# Using set and comparing sizes                
+class Solution: 
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == len(set(nums)):
+            return False
+        else:
+            return True
