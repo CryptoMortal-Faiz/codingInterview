564de67f3ad04280a8b52131811dcd635a180ee2
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-12-14 22:09:41 -0600

    Create product_except_self.py

---
+++ Leetcode-75/Array/Medium/product_except_self.py
@@ -0,0 +1,10 @@
+# Product of elements in the array excluding the index of the current element
+
+''' 2 approaches 
+1) Use two for loops and calculate product of the elements iterated in the second loop
+2) Get left elemnts product and right elements products from the index then multiply both  
+'''
+
+def product_except_self(array):
+  
+  
