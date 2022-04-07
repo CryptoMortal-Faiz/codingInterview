90041ad468d638e917d7abe3be1746220f0fa0dc
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2022-04-06 23:34:12 -0500

    Update linear_search.py

--- Algorithms/Searching/linear_search.py
+++ Algorithms/Searching/linear_search.py
@@ -1,7 +1,26 @@
-# Serch each element one by one until the target element is found 
+# Liner Search
 
-def liner_search(array, target):
-  for i in range(len(array)):
-    if array[i] == target:
-      return i
-  
+
+'''
+The time complexity of linear search is O(n)
+meaning that the time taken to execute increases
+with the number of items in our input list
+'''
+
+# Find indexes of target element 
+# Eg: Target - 9 , return first two indexes of the 9
+# [8,9,9,9,0] 
+# Output - [1,2]
+def linear_search(array,target):
+    result = []
+    for i in range(len(array)):
+        if array[i] == target:
+            result.append(i)
+            if len(result) >= 2:
+                return result
+    return [-1,-1]
+            
+    
+print(linear_search([9,9,9,0,0,0],9))
+print(linear_search([9,0,0,0,0],9))
+print(linear_search([0,0,0],9))
