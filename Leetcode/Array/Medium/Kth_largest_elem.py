5a76228931465560e731896802cae3e39592a3f3
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2022-05-01 22:08:37 -0500

    Update Kth_largest_elem.py

--- Leetcode/Array/Medium/Kth_largest_elem.py
+++ Leetcode/Array/Medium/Kth_largest_elem.py
@@ -15,3 +15,22 @@ class Solution:
     ### Apraoch 2 - Heap
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return heapq.nlargest(k, nums)[-1]
+      
+## Implementing Heap
+
+# find kth largest number in the array
+
+from heapq import *
+def kth_largest_number(arr,k):
+    
+    minheap = []
+    for i in range(k):
+        heappush(minheap, arr[i])
+    
+    for i in range(k,len(arr)):
+        if arr[i] > minheap[0]:
+            heappush(minheap,arr[i])
+    
+    return minheap[k-1]
+    
+print(kth_largest_number([1,2,3,4,5,6,9,10,13], 7))
