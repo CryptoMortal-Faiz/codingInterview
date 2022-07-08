f96fbe206711e95288e90412e3b141908db51019
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2022-07-08 12:13:24 -0400

    Use dictionary for optimal solution - Time O(n) Space O(1)

--- neetcode-150/arrays/contain_duplicate.py
+++ neetcode-150/arrays/contain_duplicate.py
@@ -4,3 +4,14 @@ class Solution:
         if len(set(nums)) == len(nums):
             return False
         return True
+    
+    # Using Dictionary
+    
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        nums_dict = {}
+        for i in nums:
+            if i in nums_dict:
+                return True
+            else:
+                nums_dict[i] = 1
